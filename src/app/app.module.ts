eb17a2cd740e70cfd8c46dea2f20a3ffd5c1f8da
Max Chernikov <max.chernikoff@gmail.com>
2017-10-26 17:13:49 +0300

    Add in-memory-service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 
+import { InMemDataService } from './in-memory-data.service';
 import { AppComponent } from './app.component';
 import { PatientsComponent } from './patients/patients.component';
 
@@ -14,7 +17,9 @@ import { PatientsComponent } from './patients/patients.component';
   imports: [
     BrowserModule,
     FormsModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemDataService),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/patients/patients.component.ts
+++ src/app/patients/patients.component.ts
@@ -1,17 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
-
-const states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado',
-  'Connecticut', 'Delaware', 'District Of Columbia', 'Federated States Of Micronesia', 'Florida', 'Georgia',
-  'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine',
-  'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana',
-  'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-  'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island',
-  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Islands', 'Virginia',
-  'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+import 'rxjs/add/operator/pluck';
 
 @Component({
   selector: 'app-patients',
@@ -21,20 +14,21 @@ const states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'C
 export class PatientsComponent implements OnInit {
 
   public model: any;
+  public patients: any;
 
-  // debounceTime operator
-  // kicks in only if 2+ characters typed
-  // limits to 10 results
   search = (text$: Observable<string>) =>
     text$
       .debounceTime(200)
       .distinctUntilChanged()
-      .map(term => term.length < 2 ? []
-        : states.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+      .map(term => term.length < 3 ? []
+        : this.patients.data.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
 
-  constructor() { }
+  constructor(private http: Http) {}
 
   ngOnInit() {
+    this.http.get('/api/patients').subscribe(data => {
+      this.patients = data.json();
+    });
   }
 
 }
